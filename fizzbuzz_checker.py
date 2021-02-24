876b4f5f10f0904c1fd0cde12c34bc684c950de2
Louis <dussoulier.louis@hotmai.fr>
2021-02-24 22:11:48 +0100

    Three commit

--- fizzbuzz_checker.py
+++ fizzbuzz_checker.py
@@ -3,6 +3,9 @@ class fizzbuzzChecker:
     @staticmethod
     def is_bob(num):
         if (num == 0):
-            raise NameError('valeur null')
+            raise NameError('valeur égal zéro')
+            return True
+        if (num < 0):
+            raise NameError('valeur négative')
             return True
         return False
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -6,3 +6,7 @@ class BisextileTest(unittest.TestCase):
     def test_should_not_be_zero(self):
         actual = fizzbuzzChecker.is_bob(1)
         self.assertFalse(actual, "Should be false")
+
+    def test_should_not_be_negative(self):
+        actual = fizzbuzzChecker.is_bob(-1)
+        self.assertFalse(actual, "Should be false")
